103c74a8e9e9ead9a9aeb49c5e993985f0be82e1
Vsollery <vsollery@gmail.com>
2023-05-06 11:44:46 +0700

    add form view

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -3,17 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Chirp;
-use Illuminate\Http\Response; 
+use Illuminate\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response; 
 
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(): Response 
+    public function index(): View 
     {
-        return response('Hello, World!');
+        return view('chirps.index');
     }
 
     /**
